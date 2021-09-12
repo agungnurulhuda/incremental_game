d8429a6a9e5a2e8a23a99e7719ebf8de01d5984e
agungnurulhuda <agungnurulhuda212@gmail.com>
2021-09-12 21:35:27 +0700

    Incremental game project

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,35 @@
+﻿/*using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+
+    }
+
+}*/
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    AudioSource collectCoint;
+    private void Start()
+    {
+        collectCoint = gameObject.GetComponent<AudioSource>();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        collectCoint.Play();
+    }
+}
